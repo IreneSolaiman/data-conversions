ca8cdfaa2925fbb0b5e1d4d64587413a33dbd660
Irene Solaiman <irs623@student.hks.harvard.edu>
2018-01-11 15:11:48 -0500

    add veggies and json

--- printveggies.py
+++ printveggies.py
@@ -1,12 +1,12 @@
 vegetables = [
- {"name": "eggplant"},
- {"name": "tomato"},
- {"name": "corn"},
- {"name": "zucchini"},
- {"name": "artichoke"},
- {"name": "butternut squash"},
- {"name": "mushroom"},
- {"name": "broccoli"},
+ {"name": "eggplant", "color": "purple"},
+ {"name": "tomato", "color": "red"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "zucchini", "color": "green"},
+ {"name": "artichoke", "color": "green"},
+ {"name": "butternut squash", "color": "yellow"},
+ {"name": "mushroom", "color": "brown"},
+ {"name": "broccoli", "color": "green"},
 ]
 
 #Write header file to csv
@@ -14,11 +14,12 @@ import csv
 
 with open('veggies.csv', 'w') as f:
 	writer = csv.writer(f)
-	writer.writerow(['name', 'length'])
+	writer.writerow(['name', 'color', 'length'])
 
 #Loop through each veggie
 	for veggie in vegetables:
 #For each veggie write a row to the csv
 		name = veggie["name"]
+		color = veggie["color"]
 		length = len(name)
-		writer.writerow([name, length])
+		writer.writerow([name, color, length])
